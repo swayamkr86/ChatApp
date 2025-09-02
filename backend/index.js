4f98443f49460aca62e90d3938144578289a8adb
Swayam <137387677+swayamkr86@users.noreply.github.com>
2025-09-03 03:13:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://chatapp-xsug.onrender.com",
     credentials:true
 }))
 app.use(express.json())
