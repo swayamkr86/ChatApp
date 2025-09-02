2fda3819e5e1604df620389bc6985de23084be8a
Swayam <137387677+swayamkr86@users.noreply.github.com>
2025-09-03 03:04:32 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import { store } from './redux/store.js'
 
-export const serverUrl="http://localhost:8000"
+export const serverUrl="https://chatapp-backend-g6yf.onrender.com"
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <Provider store={store}>
